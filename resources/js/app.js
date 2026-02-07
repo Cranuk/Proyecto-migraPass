d0c419ab6e07106d32c2affc991a840adf650ba4
Cranuk <facundom4t0@gmail.com>
2026-02-06 23:11:56 -0300

    Se optimizo la card de detalles y se mejoro los botones de mostrar clave y copiado al portapapeles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+import ClipboardJS from 'clipboard';
+
+// Inicializamos ClipboardJS de forma global
+const clipboard = new ClipboardJS('.btn-copy');
+
+clipboard.on('success', function(e) {
+    console.log('Texto copiado:', e.text);
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Error al copiar:', e.action);
+});
--- resources/views/livewire/details-app.blade.php
+++ resources/views/livewire/details-app.blade.php
@@ -1,35 +1,39 @@
-{{-- NOTE: mover esto a la card donde se vera la info de la aplicacion
-
---}}
-
 <div class="details-app">
     <h3>Detalles de la aplicacion</h3>
     <div class="details-data">
         <label class="label-text">Nombre de la aplicacion:</label>
-        <span class="label-text">{{$name}}</span>
+        <div class="label-text-plus">
+            <p class="label-text">{{$name}}</p>
+            @include('includes.buttons.button-go-page')
+        </div>
     </div>
     <div class="details-data">
         <label class="label-text">Usuario:</label>
         <div class="label-text-plus">
-            <span class="label-text">{{$user_aplication}}</span>
+            <p class="label-text">{{$user_aplication}}</p>
+            <button type="button" class="button cancel small btn-copy" data-clipboard-text="{{ $user_aplication }}" title="Copiar usuario">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-copy">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 9.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667l0 -8.666" />
+                    <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg>
+            </button>
         </div>
     </div>
     <div class="details-data">
         <label class="label-text">Contraseña:</label>
         <div class="label-text-plus">
-            <span class="label-text">{{ $showPassword ? $password_aplication : str_repeat('•', 10) }}</span>
+            <p class="label-text">{{ $showPassword ? $password_aplication : str_repeat('•', 10) }}</p>
             @include('includes.buttons.button-show-password')
-        </div>
-    </div>
-    <div class="details-data">
-        <label class="label-text">URL:</label>
-        <div class="label-text-plus">
-            <span class="label-text">{{$url_aplication}}</span>
-            @include('includes.buttons.button-go-page')
+            <button type="button" class="button cancel small btn-copy" data-clipboard-text="{{ $password_aplication }}" title="Copiar contraseña">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-copy">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 9.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667l0 -8.666" />
+                    <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg>
+            </button>
         </div>
     </div>
     <div class="details-data">
         <label class="label-text">Notas:</label>
-        <span class="label-text">{{$notes}}</span>
+        <p class="label-text">{{$notes}}</p>
     </div>
 </div>
