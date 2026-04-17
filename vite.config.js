70595bfe0b7d7027fec14edde3ebe6de3106b0c3
fmato <fmato@cecaitra.org.ar>
2026-04-17 04:30:24 +0000

    Modificaciones menores para que sea funcional en windows

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5176,
         hmr: {
-            host: 'migrapass.test'
+            host: 'localhost',
         },
     },
 });
